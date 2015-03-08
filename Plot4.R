66b235848cb84cc236b2c5eb414e2b45d8d23c17
jscaze <jscaze@users.noreply.github.com>
2015-03-08 22:43:28 +0100

    add comments

--- Plot4.R
+++ Plot4.R
@@ -5,13 +5,13 @@ create_plot4 <- function(directory) {
   
   #set the directory in parameter as the working folder
   setwd(directory)
-  
+  #check if the file exists in the folder data
   if(!file.exists("./data/household_power_consumption.txt")){
     print("Directory not correct, please input the path where the file <household_power_consumption.txt> is.")
   } 
   else{
     
-    #load the activity and name the column
+    #load the household_power_consumption.txt file
     dfHousePower <- read.table("./data/household_power_consumption.txt", sep=";", 
                                header =TRUE, na.strings = "?")
     
@@ -23,18 +23,18 @@ create_plot4 <- function(directory) {
         as.Date(dfHousePower$Date,"%d/%m/%Y") == 
         as.Date("2007-02-02", "%Y-%m-%d")  
       ,]
-    
+    #add a column in the DF to get the DATE + TIME in one column
     dfSubHousePower$Timestamp <- paste (as.Date(dfSubHousePower$Date,"%d/%m/%Y"), 
                                         dfSubHousePower$Time, 
                                         sep = " ", collapse = NULL)
-    
+    #create the graph in PNG file
     png("plot4.png", width = 480, height = 480, units = "px", bg = "white")
     
-    #par(mfrow = c(1, 4), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+    #to get the graph on 2 columns and 2 lines
     par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
-    
+    #all graphs
     with(dfSubHousePower,
-        {
+        { #first graph
           plot( 
             x=strptime(dfSubHousePower$Timestamp,"%Y-%m-%d %H:%M:%S"),
             y=dfSubHousePower$Global_active_power,
@@ -44,7 +44,7 @@ create_plot4 <- function(directory) {
             ylab="Global Active Power (killowatts)",
             #xaxt="n"
           )
-          
+          #second graph
           plot( 
             x=strptime(dfSubHousePower$Timestamp,"%Y-%m-%d %H:%M:%S"),
             y=dfSubHousePower$Voltage,
@@ -54,7 +54,7 @@ create_plot4 <- function(directory) {
             ylab="Voltage",
             #xaxt="n"
           )
-          
+          #third graph
           {
                 plot( 
                     x=strptime(dfSubHousePower$Timestamp,"%Y-%m-%d %H:%M:%S"),
@@ -78,7 +78,7 @@ create_plot4 <- function(directory) {
                     legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3")
                     )
           }
-       
+          #last graph
           plot( 
             x=strptime(dfSubHousePower$Timestamp,"%Y-%m-%d %H:%M:%S"),
             y=dfSubHousePower$Global_reactive_power,
