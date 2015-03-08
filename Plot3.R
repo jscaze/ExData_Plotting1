55c9818b015734c0e6ab63f0ffc971f499f37b23
jscaze <jscaze@users.noreply.github.com>
2015-03-08 22:40:00 +0100

    add comment

--- Plot3.R
+++ Plot3.R
@@ -5,13 +5,13 @@ create_plot3 <- function(directory) {
   
   #set the directory in parameter as the working folder
   setwd(directory)
-  
+  #check if the file exists in the folder data
   if(!file.exists("./data/household_power_consumption.txt")){
     print("Directory not correct, please input the path where the file <household_power_consumption.txt> is.")
   } 
   else{
     
-    #load the activity and name the column
+    #load the household_power_consumption.txt file
     dfHousePower <- read.table("./data/household_power_consumption.txt", sep=";", 
                                header =TRUE, na.strings = "?")
     
@@ -23,15 +23,15 @@ create_plot3 <- function(directory) {
         as.Date(dfHousePower$Date,"%d/%m/%Y") == 
         as.Date("2007-02-02", "%Y-%m-%d")  
       ,]
-    
+    #add a column in the DF to get the DATE + TIME in one column
     dfSubHousePower$Timestamp <- paste (as.Date(dfSubHousePower$Date,"%d/%m/%Y"), 
                                         dfSubHousePower$Time, 
                                         sep = " ", collapse = NULL)
-    
+    #to get a PNG
     png("plot3.png", width = 480, height = 480, units = "px", bg = "white")
     
     par(mar= c(4, 4, 2, 1))
-    
+    #draw the plot
     with(dfSubHousePower,
           {
           plot( 
@@ -44,24 +44,24 @@ create_plot3 <- function(directory) {
                 ylab="Energy sub metering"
                 #xaxt="n"
                 )
-
+         #add another line
          lines( 
            x=strptime(dfSubHousePower$Timestamp,"%Y-%m-%d %H:%M:%S"),
            y=dfSubHousePower$Sub_metering_2,
            col = "red")
-    
+         #add another line
          lines( 
            x=strptime(dfSubHousePower$Timestamp,"%Y-%m-%d %H:%M:%S"),
            y=dfSubHousePower$Sub_metering_3,
            col = "blue")
-    
+          #add a legend
           legend("topright", pch = "_", 
             col = c("black", "red", "blue"), 
             legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3")
            )
           
           })  
-    
+    #create the graph in PNG file
     dev.off()
   }
 }
